a2767e1d0a533a74a0042e14ceb96a72247ea4f2
KVRenaissance <kyle@roguex.co.za>
2025-04-17 23:30:51 +0200

    Buttons imported into main.py with PhotoImage function

--- main.py
+++ main.py
@@ -11,4 +11,9 @@ pygame.mixer.init()
 songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
 songlist.pack()
 
+play_btn_image = PhotoImage(file="play.png")
+pause_btn_image = PhotoImage(file="pause.png")
+next_btn_image = PhotoImage(file="next.png")
+prev_btn_image = PhotoImage(file="prev.png")
+
 root.mainloop()
