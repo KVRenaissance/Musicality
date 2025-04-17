091bc773fa4b77a837961f7741afeffd7dd4cc7b
KVRenaissance <kyle@roguex.co.za>
2025-04-17 23:29:02 +0200

    Musicality base interface + images in root folder

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from tkinter import *
 import pygame
+import os
+
+root = Tk()
+root.title("Musicality")
+root.geometry("500x300")
+
+pygame.mixer.init()
+
+songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
+songlist.pack()
+
+root.mainloop()
