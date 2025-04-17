59a6605e1c9b5babbd94824179122675108d0ce2
KVRenaissance <kyle@roguex.co.za>
2025-04-17 23:38:53 +0200

    Buttons added to interface in grid, no functionality yet. Click works.

--- main.py
+++ main.py
@@ -19,4 +19,14 @@ prev_btn_image = PhotoImage(file="prev.png")
 control_frame = Frame(root)
 control_frame.pack()
 
+play_btn = Button(control_frame, image=play_btn_image, borderwidth=0)
+pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0)
+next_btn = Button(control_frame, image=next_btn_image, borderwidth=0)
+prev_btn = Button(control_frame, image=prev_btn_image, borderwidth=0)
+
+play_btn.grid(row=0, column=0, padx=7, pady=10)
+pause_btn.grid(row=0, column=1, padx=7, pady=10)
+next_btn.grid(row=0, column=2, padx=7, pady=10)
+prev_btn.grid(row=0, column=3, padx=7, pady=10)
+
 root.mainloop()
